acaf794c0e392f86186b321b72d03f95b5874fa3
Zach Anderson <zach.inbox@gmail.com>
2016-03-12 13:24:17 -0600

    Cleaned out main

--- source/main.cpp
+++ source/main.cpp
@@ -3,36 +3,13 @@
 #include <iostream>
 #include <unistd.h>
 
+#include "Piano.h"
+#include "Harp.h"
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	ALSA output;
-	for(unsigned int n = 0; n < 3; n++)
-	{
-		for(unsigned int i = 0; i < 3; i++)
-		{
-			output.noteOn(72,127);
-			output.noteOn(76,127);
-			output.noteOn(79,127);
-			sleep(1);
-			output.noteOff(72);
-			output.noteOff(76);
-			output.noteOff(79);
-			output.noteOn(72,127);
-			output.noteOn(76,127);
-			output.noteOn(81,127);
-			sleep(1);
-			output.noteOff(72);
-			output.noteOff(76);
-			output.noteOff(81);
-		}
-		output.noteOn(72,127);
-		output.noteOn(76,127);
-		output.noteOn(79,127);
-		sleep(2);
-		output.noteOff(72);
-		output.noteOff(76);
-		output.noteOff(79);
-	}
+	Piano piano(output);
 }
+
